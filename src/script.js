20f80e2821afd103c2e205725abdd2e035f09633
biswasindhu.mandal <exambiswa10@gmail.com>
2022-07-19 19:28:36 +0530

    update UI

--- src/script.js
+++ src/script.js
@@ -1,51 +1,63 @@
 function createRegex(digit) {
     digit = Number(digit);
     return {
-        uptoOneDecimal: new RegExp(`[\\+\\-\\d]*\.[\\d]{${digit - 1}}`, 'g'),
-        twoDecimalDigit: new RegExp(`(?<=[\\d]*\\.[\\d]{${digit - 1}})[\\d]`, 'g'),
-        threeDecimalDigit: new RegExp(`(?<=[\\d]*\\.[\\d]{${digit}})[\\d]`, 'g'),
-        anyNonZeroDigitsAfterThreeDecimalDigit: new RegExp(`(?<=[\\d]*\\.[\\d]{${digit + 1},})[1-9]`, 'g'),
-        noOfDecimalDigits: new RegExp(`(?<=[\\d]*[\\.])[\\d]*`, 'g'),
+        roundOffPlace: digit,
+        beforeRounOffDigit_regx: new RegExp(`[\\+\\-\\d]*\.[\\d]{${digit - 1}}`, 'g'),
+        roundOffDigit_regx: new RegExp(`(?<=[\\d]*\\.[\\d]{${digit - 1}})[\\d]`, 'g'),
+        firstDiscardDigit_regx: new RegExp(`(?<=[\\d]*\\.[\\d]{${digit}})[\\d]`, 'g'),
+        anyNonZeroDiscardDigit_regx: new RegExp(`(?<=[\\d]*\\.[\\d]{${digit + 1},})[1-9]`, 'g'),
+        noOfDecimalDigits: new RegExp(`(?<=\\d*\\.)[\\d]*`, 'g'),
         noOfIntegerDigits: new RegExp(`[\\d]+(?=[\\.][\\d]+)`, 'g'),
-        checkSign: new RegExp(`[\\+\\-](?=[\\.][\\d]+)`, 'g'),
-        oddRegex: new RegExp(`[13579]`, 'g')
+        checkSign: new RegExp(`[\\+\\-](?=[\\.][\\d]+)`, 'g')
     };
 }
 
-const arrList = ['0.003', '.005', '0.0050001', '7.2550000190', '7.255', '7.2550000',
+addDecimalDigits = ((d) => {
+    let s = '';
+    for (let i = 0; i < d; i++) s += '0';
+    return s;
+})
+
+const arrList = ['0.003', '9.9999999999999999', '.005', '0.0050001', '7.2550000190', '7.255', '7.2550000',
     '12.992', '12340.999', '123.995001', '999.99612322', '7.09612322',
     '1234.', '6789', ".", -12.346, "-.2345", "-.", '6.0422', '6.4872',
-    '6.997', '6.06500', '7.485', '6.755000', '8.995', '6.06501', '7.4852007', ''];
+    '6.997', '6.06500', '7.485', '6.755000', '8.995', '6.06501', '7.4852007', '', '0'];
 
-function roundOffRules(num) {
-    // Validate Case for this type of input: "1234." or "."
-    const d = num.match(regexInfo.noOfDecimalDigits)?.toString().length;
-    num = num + (d === undefined ? '.0' : d === 0 ? '0' : '');
-
-    const uptoSingleDigit = num.match(regexInfo.uptoOneDecimal)?.[0];
-    const nth = Number(num.match(regexInfo.twoDecimalDigit)?.[0]);
-    const np1th = Number(num.match(regexInfo.threeDecimalDigit)?.[0]);
-    const np1next = Number(num.match(regexInfo.anyNonZeroDigitsAfterThreeDecimalDigit)?.[0]);
-    const nth_idOdd = /[13579]/g.test(nth);
-    const carry = !np1th ? 0 : np1th > 5 ? 1 : np1th < 5 ? 0 : np1next ? 1 : nth_idOdd ? 1 : 0;
-    const rule = !np1th ? 'GR' : np1th > 5 ? 'R1' : np1th < 5 ? 'R2' : np1next ? 'R3' : nth_idOdd ? 'R4' : 'R5';
-    // console.log(uptoSingleDigit, nth, np1th, np1next, '(',nth_idOdd,')', carry);
-
-    let roundOffValue;
-    if (nth === 9 && carry) {
-        roundOffValue = (Number(`${uptoSingleDigit}` + `${nth ? nth : 0}`) + Number(`0.0${carry}`)).toString();
-    } else {
-        roundOffValue = (uptoSingleDigit + ((nth ? nth : 0) + carry)).toString();
+function roundOffRules(num, isDefaultData = false) {
+    if (!num || !/\d/g.test(num)) {
+        return `<strong>-</strong>`;
     }
+    // Validate Case for this type of input: "1234." or "."
+    let roundOffValue;
+    if(num.match(regexInfo.noOfDecimalDigits)?.toString().length > regexInfo.roundOffPlace) {
+        const beforeRounOffDigit = num.match(regexInfo.beforeRounOffDigit_regx)?.[0];
+        const roundOffDigit = Number(num.match(regexInfo.roundOffDigit_regx)?.[0]);
+        const firstDiscardDigit = Number(num.match(regexInfo.firstDiscardDigit_regx)?.[0]);
+        const anyNonZeroDiscardDigit = Number(num.match(regexInfo.anyNonZeroDiscardDigit_regx)?.[0]);
+        const idOddRoundOffDigit = /[13579]/g.test(roundOffDigit);
+        const carry = !firstDiscardDigit ? 0 : firstDiscardDigit > 5 ? 1 : firstDiscardDigit < 5 ? 0 : anyNonZeroDiscardDigit ? 1 : idOddRoundOffDigit ? 1 : 0;
+        const rule = !firstDiscardDigit ? 'GR' : firstDiscardDigit > 5 ? 'R1' : firstDiscardDigit < 5 ? 'R2' : anyNonZeroDiscardDigit ? 'R3' : idOddRoundOffDigit ? 'R4' : 'R5';
+        // console.log(beforeRounOffDigit, roundOffDigit, firstDiscardDigit, anyNonZeroDiscardDigit, '(',idOddRoundOffDigit,')', carry);
+
+        if (roundOffDigit === 9 && carry) {
+            roundOffValue = (Number(`${beforeRounOffDigit}` + `${roundOffDigit ? roundOffDigit : 0}`) + Number(`0.${addDecimalDigits(regexInfo.roundOffPlace - 1)}${carry}`)).toString();
+        } else {
+            roundOffValue = (beforeRounOffDigit + ((roundOffDigit ? roundOffDigit : 0) + carry)).toString();
+        }
+    } else {
+        roundOffValue = num;
+        rule = "GN";
+    }
+
     // Beaufity number upto two decimal places
     const dd = roundOffValue.match(regexInfo.noOfDecimalDigits)?.toString().length;
-    roundOffValue = roundOffValue + (dd === undefined ? '.00' : dd === 1 ? '0' : '');
+    roundOffValue = roundOffValue + (dd === undefined ? `.${addDecimalDigits(regexInfo.roundOffPlace)}` : dd < regexInfo.roundOffPlace ? addDecimalDigits(regexInfo.roundOffPlace - dd) : '');
     const id = roundOffValue.match(regexInfo.noOfIntegerDigits)?.toString().length;
     if (id === undefined) {
         roundOffValue = roundOffValue.match(regexInfo.checkSign)?.length ? roundOffValue.replace(regexInfo.checkSign, '$&0') : ('0' + roundOffValue);
     }
 
-    console.log(`${roundOffValue} (${num})`);
+    console.log(`${roundOffValue} (${num}) --- ${isDefaultData}`);
     return `<strong>${roundOffValue}</strong> ${rule ? ('<span style="color:red">(<strong>' + rule + '</strong>)</span>') : ''}`;
 }
 
@@ -58,7 +70,7 @@ function getData(arr) {
     arr.forEach(ele => {
         result += `<tr>
                         <td><strong>${ele}</strong></td>
-                        <td>${roundOffRules(ele.toString())}</td>
+                        <td>${roundOffRules(ele.toString(), true)}</td>
                     </tr>`;
     });
     return result + `</table>`;
@@ -67,7 +79,7 @@ function getData(arr) {
 /**
  * Create Regular Expressions
  */
-let regexInfo = createRegex(2);
+let regexInfo = createRegex(3);
 
 /**
  * Main Function
@@ -84,15 +96,17 @@ function onSubmit() {
 /**
 *  Show Sample Data
 */
-// function onDPSubmit() {
-//     const n2 = document.getElementById("dcPlace").value;
-//     regexInfo = createRegex(n2 ? n2 : 2);
-//     document.getElementById('sample_data').innerHTML = `<h3>Example (2 Decimal Places)</h3><hr>${roundOffRules(arrList, true)}`;
-// }
+function onDPSubmit() {
+    const n2 = document.getElementById("dcPlace").value;
+    if (n2) {
+        regexInfo = createRegex(Number(n2));
+        document.getElementById('sample_data').innerHTML = getData(arrList);
+        onSubmit();
+    }
+}
 
 /**
 *  Default Call during page loading time
 */
-createRegex(2);
 document.getElementById('sample_data').innerHTML = getData(arrList);
 onSubmit();
